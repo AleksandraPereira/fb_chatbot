e497c3ad539eaf934c48000350de791cf193f30f
AleksandraPereira <121905245+AleksandraPereira@users.noreply.github.com>
2024-03-14 17:34:57 -0300

    Commits-finais-corrigidas

--- src/main/java/br/com/ubots/chatbot/ChatBotController.java
+++ src/main/java/br/com/ubots/chatbot/ChatBotController.java
@@ -5,27 +5,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 @RestController
 public class ChatBotController {
     private static final String VERIFY_TOKEN = "batata";
 
     @GetMapping("/webhook")
-    public String verify(@RequestParam("hub.verify_token") String verifyToken, @RequestParam("hub.challenge")
+    public String verify( @RequestParam("hub.verify_token") String verifyToken, @RequestParam("hub.challenge")
                          String challenge) {
-        if (verifyToken.equals(VERIFY_TOKEN)) {
+        if (Objects.equals(VERIFY_TOKEN, verifyToken)) {
             return challenge;
         } else {
-            return "Falha na verificação do token";
+            return String.valueOf(Integer.valueOf("Falha na verificação do token"));
 
         }
     }
 
-    /*
+
     @PostMapping("/webhook")
     public ResponseEntity<String> webhook(@RequestBody String message) {
       System.out.println(message);
     return ResponseEntity.status(HttpStatus.OK).body("OK");
-    */
+    }
 }
-
 
---
+++ src/main/java/br/com/ubots/chatbot/Main.java
@@ -0,0 +1,29 @@
+package br.com.ubots.chatbot;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+        public static void main(String[] args) throws Exception {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server.createContext("/webhook", new MyHandler());
+            server.setExecutor(null);
+            server.start();
+        }
+    
+        static class MyHandler implements HttpHandler {
+            @Override
+            public void handle(HttpExchange t) throws IOException {
+                String response = "This is the response";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        }
+    }
