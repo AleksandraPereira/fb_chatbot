8ff00979e2c2f123fe18466e5d4b14134faa0225
AleksandraPereira <121905245+AleksandraPereira@users.noreply.github.com>
2024-03-25 13:17:41 -0300

    feat:commit com regex e clima funcionando com melhorias, sem comentários para guiar.

--- src/main/java/br/com/ubots/chatbot/utils/ConversaoKelvinCelcius.java
+++ src/main/java/br/com/ubots/chatbot/utils/ConversaoKelvinCelcius.java
@@ -5,11 +5,9 @@ import com.google.gson.JsonObject;
 
 public class ConversaoKelvinCelcius {
     public String converte(String response) {
-        // Suponha que a resposta seja um JSON e que a temperatura em Kelvin esteja armazenada no campo "temp".
         JsonObject jsonObject = new Gson().fromJson(response, JsonObject.class);
         double tempKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
 
-        // Convertendo a temperatura de Kelvin para Celsius.
         double tempCelsius = tempKelvin - 273.15;
 
         return String.format("A temperatura é %.0f°C", tempCelsius);
